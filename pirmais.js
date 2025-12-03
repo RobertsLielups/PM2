8e5f773c177e8ac99c29631cb040fe64abe51d51
Your Name <YOUR_VERIFIED_EMAIL@EXAMPLE.COM>
2025-12-03 18:35:28 +0200

    my first project without js

--- pirmais.js
+++ pirmais.js
@@ -1,3 +1,41 @@
+console.log('esam iekša')
+console.log(
+    "Šī ir teksta rinda".toUpperCase()
+)
+console.log(
+    123 + 456 * 5 *3 /6 / 3 - 500
+)
+console.log(
+    false
+)
+console.log['teksts 1','teksts 2',2, 4]
 
+console.log(
+    {
+        name: 'Roberts',
+        lastName: 'Bambuzo',
+        age: 212
+    }
+)
 
+const animals = ['cat','dog','fish']
+
+console.log(animals[1])
+
+const dog = {
+    name: 'reksis',
+    age: 2,
+    gender: 'male'
+}
+console.log(dog.gender)
+
+let number1 = 123
+let number2 = 33
+
+console.log(number1+number2)
+
+number = 5
+console.log(number + number2)
+
+const pi = 3.13
 
